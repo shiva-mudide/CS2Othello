d89b92fcb0b1ebb80d861f0ecb9342c5803b569b
James Wei <jameswei@Jamess-MacBook-Pro.local>
2018-03-04 23:34:33 -0800

    Made changes to player.cpp

--- player.cpp
+++ player.cpp
@@ -1,5 +1,7 @@
 #include "player.hpp"
 
+// Hi, my name is James.
+
 /*
  * Constructor for the player; initialize everything here. The side your AI is
  * on (BLACK or WHITE) is passed in as "side". The constructor must finish
