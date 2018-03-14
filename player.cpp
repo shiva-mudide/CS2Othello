c41a55475ef9c31e7fe43367b8409d5be06f9a3b
shiva-mudide <shivamudide628@gmail.com>
2018-03-13 17:04:37 -0700

    Tournament Ready Bot. Please use this commit if max time per game is 16 minutes. Minimax depth is set to 7.

--- player.cpp
+++ player.cpp
@@ -420,7 +420,7 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
      * process the opponent's opponents move before calculating your own move
      */
 
-    int minimaxDepth = 5;
+    int minimaxDepth = 7;
     // this can be changed to the desired value
 
     board -> doMove(opponentsMove, opp_side);
